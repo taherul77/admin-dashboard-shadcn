09db6bbfb73082089c1a1281a0c1f4ddd01fb35c
SK Taherul Islam Taz <taherul15-2317@diu.edu.bd>
2025-04-28 10:12:45 +0600

    update

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -47,9 +47,9 @@ export function NavUser({
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground mt-[-10px]"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
+              <Avatar className="h-8 w-8 rounded-lg ">
                 <AvatarImage src={user.avatar} alt={user.name} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
